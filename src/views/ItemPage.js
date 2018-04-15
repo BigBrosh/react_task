c7d1841bed710774c9afad8fa1194d8177fd9905
BigBrosh <30599468+BigBrosh@users.noreply.github.com>
2018-04-15 16:57:37 +0300

    imgs' styles modified

--- src/views/ItemPage.js
+++ src/views/ItemPage.js
@@ -7,7 +7,13 @@ export class ItemPage extends React.Component {
 			<div>
 				<p>{item.price_currency} {item.price}</p>
 				<p>{item.title}</p>
-				<img src={item.img_url} height={item.img_height} width={item.img_width} />
+
+				<img 	src={item.img_url} 
+						style={{
+							maxWidth: item.img_width, 
+							width: '100%'
+						}} />
+
 				<p>{item.bedroom_number} bed, {item.bathroom_number} bathrooms</p>
 				<p>{item.summary}</p>
 			</div>
