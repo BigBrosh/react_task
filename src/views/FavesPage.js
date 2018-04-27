57edc370177ec92b185789632df93561550497c7
BigBrosh <30599468+BigBrosh@users.noreply.github.com>
2018-04-27 22:03:50 +0300

    faves page modified

--- src/views/FavesPage.js
+++ src/views/FavesPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {Link} from 'react-router';
+import {Link} from 'react-router-dom';
 
 import {Header} from '../header/Header';
 
 // controllers
 import {RequestController} from '../controllers/RequestController';
 import {ItemController} from '../controllers/ItemController';
+import {DataFromLink} from '../helpers/DataFromLink.js';
+import {CustomLink} from '../helpers/CustomLink';
 
 // styles
 import {styles} from '../styles/mainStyles';
@@ -25,19 +27,26 @@ export class FavesPage extends React.Component {
 		if (list && list.length > 0)
 		{
 			result = list.map(el => {
+				let	place = DataFromLink.find(el.url, 'place_name'),
+					page = DataFromLink.find(el.url, 'page');
+
+				let url = `item/sv=${place}&pg=${page}&num=${el.numberInList}`;
+
 				return (
 					<li 	key={`${el.numberInList}${el.index}`}
 							data-index={el.index}
 							data-numberinlist={el.numberInList}
 							style={styles.itemList.listItem}>
-						<div>
-							<img 	alt={el.place}
-									src={el.image}
-									style={{maxWidth: 150}}/>
-						</div>
-						<div>
-							<p>{el.price}<br/>{el.place}</p>
-						</div>
+						<Link to={`/${url}`}>
+							<div>
+								<img 	alt={el.place}
+										src={el.image}
+										style={{maxWidth: 150}}/>
+							</div>
+							<div>
+								<p>{el.price}<br/>{el.place}</p>
+							</div>
+						</Link>
 					</li>
 				); 
 			});
